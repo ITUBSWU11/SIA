f6413641a330e4b257b14e66558ce777febb25da
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-10-04 09:21:40 +0200

    initial push

---
+++ src/sia/SIA.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sia;
+
+/**
+ *
+ * @author hypesystem
+ */
+public class SIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("LOL");
+    }
+}
